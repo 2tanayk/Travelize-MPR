0bdc5fcd78eba0908a5b7fad2f32356a34ae8deb
Tanay Kamath <2tanaykamath@gmail.com>
2021-04-09 13:33:20 +0530

    add profile fragment2 UI :sparkles:

--- app/src/main/java/com/myapp/travelize/main/createprofile/CreateProfileFragment2.kt
+++ app/src/main/java/com/myapp/travelize/main/createprofile/CreateProfileFragment2.kt
@@ -1,14 +1,22 @@
 package com.myapp.travelize.main.createprofile
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.myapp.travelize.R
+import com.myapp.travelize.main.MainHostActivity
 
 class CreateProfileFragment2 : Fragment() {
-
+    lateinit var passionChipGroup: ChipGroup
+    lateinit var nextBtn2: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,5 +28,41 @@ class CreateProfileFragment2 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_profile2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val context = activity as? MainHostActivity
+        val passions = listOf<String>(
+            "Cycling",
+            "Jogging",
+            "Dancing",
+            "Drawing",
+            "Football",
+            "Music",
+            "Climbing",
+            "Stand up Comedy",
+            "Cooking",
+            "Swimming"
+        )
+        passionChipGroup = view.findViewById(R.id.passionChipGroup)
+        nextBtn2 = view.findViewById(R.id.continueBtn2)
+        for (index in passions.indices) {
+            val chip = Chip(activity)
+            chip.text = passions[index]
+            chip.tag = passions[index]
+            // necessary to get single selection working
+            chip.isClickable = true
+            chip.checkedIcon = ContextCompat.getDrawable(requireActivity(), R.drawable.ic_chipcheck)
+            chip.isCheckable = true
+            chip.isCheckedIconVisible = true
+            passionChipGroup.addView(chip)
+        }
+
+        nextBtn2.setOnClickListener {
+            context?.replaceFragment(CreateProfileFragment3())
+            Log.e("Selected chip id", passionChipGroup.checkedChipId.toString())
+            Log.e("Selected chips", passionChipGroup.checkedChipIds.toString())
+        }
     }
 }
