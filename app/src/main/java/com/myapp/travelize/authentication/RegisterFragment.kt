f49c0c8037cf689c0c6779bc01f005f5ce75e87b
Fr1x3 <gathufx@gmail.com>
2021-04-22 15:34:11 -0700

    signup button enabled-disabled

--- app/src/main/java/com/myapp/travelize/authentication/RegisterFragment.kt
+++ app/src/main/java/com/myapp/travelize/authentication/RegisterFragment.kt
@@ -2,7 +2,9 @@ package com.myapp.travelize.authentication
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
 import android.util.Patterns
 import androidx.fragment.app.Fragment
@@ -24,6 +26,7 @@ class RegisterFragment : Fragment() {
     lateinit var nameEditText: TextInputEditText
     lateinit var passwordEditText: TextInputEditText
     lateinit var confirmPasswordEditText: TextInputEditText
+    private lateinit var  signupBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,12 +46,15 @@ class RegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val toolbar = view.findViewById<Toolbar>(R.id.backToolBar)
-        val signupBtn = view.findViewById<Button>(R.id.signupBtn)
+        signupBtn = view.findViewById<Button>(R.id.signupBtn)
         auth = FirebaseAuth.getInstance()
         nameEditText = view.findViewById(R.id.nameEditText)
         emailEditText = view.findViewById(R.id.emailEditText)
         passwordEditText = view.findViewById(R.id.passwordEditText)
         confirmPasswordEditText = view.findViewById(R.id.confirmEditText)
+
+        //disable button
+        signupBtn.isEnabled = false
 
         activity?.setActionBar(toolbar)
         activity?.title = ""
@@ -101,6 +107,27 @@ class RegisterFragment : Fragment() {
         return true
     }
 
+    //textwatcher
+    val textWatcher : TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            //
+            val name = getText(nameEditText)
+            val email = getText(emailEditText)
+            val password = getText(passwordEditText)
+            val confirmPassword = getText(confirmPasswordEditText)
+
+            // eabled when all texts are filled
+            signupBtn.isEnabled =
+                ( !TextUtils.isEmpty(name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(
+                    password
+                ) || !TextUtils.isEmpty(confirmPassword) )
+        }
+
+        override fun afterTextChanged(s: Editable?) {  }
+
+    }
 
     private fun signup(email: String, password: String, name: String) {
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
