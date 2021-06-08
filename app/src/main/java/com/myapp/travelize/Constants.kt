830dd38d26582fb9e1d559c14de8e07265be6553
Tanay Kamath <2tanaykamath@gmail.com>
2021-06-09 01:48:25 +0530

    add more editing for user's profile page

--- app/src/main/java/com/myapp/travelize/Constants.kt
+++ app/src/main/java/com/myapp/travelize/Constants.kt
@@ -5,5 +5,9 @@ class Constants {
         const val ACTION_KEY="action_key"
         const val ACTION_CHAT_GROUP_SELECTED=1
         const val ACTION_EDIT_PASSIONS=2
+        const val ACTION_EDIT_PFP=3
+        const val ACTION_EDIT_PFP_DELETE=4
+        const val ACTION_EDIT_PFP_GALLERY=5
+        const val ACTION_EDIT_PFP_CAMERA=6
     }
 }
